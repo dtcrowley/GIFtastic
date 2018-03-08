6841ca91ba6c461dc5be00ca066dc5352af1246e
dtcrowley <crowleyd1988@gmail.com>
2018-03-07 21:57:23 -0500

    Can't stop watching cat gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,54 @@ $(document).ready(function(){
       var topics = ["Red Hot Chili Peppers", "Pearl Jam", "Wu Tang Clan", "Jimi Hendrix", "Pink Floyd", "Fleetwood Mac", "Nirvana", "Johnny Cash",
     "Prince", "Eric Clapton"];
 
+   function displayBandInfo () {
+    // Grabbing and storing the data-name property value from the button
+        var bandInfo = $(this).data("name");
+
+    // Constructing a queryURL using the band/artist name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        bandInfo + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Performing an AJAX request with the queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+
+        //Deleting previously generated GIFs so that the new button-click is the only result displayed
+        $("#gifs-go-here").empty();
+        
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+
+            // Creating and storing a div tag
+            var bandsDiv = $("<div>");
+
+            // Creating a paragraph tag with the result item's rating
+            var para = $("<p>").text("Rating: " + results[i].rating);
+
+            // Creating and storing an image tag
+            var bandImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            bandImage.attr("src", results[i].images.fixed_height.url);
+
+            // Appending the paragraph and image tag to the bandsDiv
+            bandsDiv.append(para);
+            bandsDiv.append(bandImage);
+
+            // Prepending the bandsDiv to the HTML page in the "#gifs-go-here" div
+            $("#gifs-go-here").prepend(bandsDiv);
+
+        }
+
+        });
+
+    };
+
+
       //Function for displaying buttons
       function renderButtons() {
       
@@ -22,6 +70,7 @@ $(document).ready(function(){
           bandName.text(topics[i]);
           // Adding the button to the HTML
           $("#buttons-go-here").append(bandName);
+         
         }
       }
       // This function handles events where one button is clicked
@@ -38,47 +87,8 @@ $(document).ready(function(){
         renderButtons();
       });
 
+      $(document).on("click", ".band", displayBandInfo);
+
       // Calling the renderButtons function at least once to display the initial list of bands/artists
       renderButtons();
-
-    $("button").on("click", function() {
-    // Grabbing and storing the data-name property value from the button
-        var bandInfo = $(this).data("name");
-
-    // Constructing a queryURL using the band/artist name
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        bandInfo + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    // Performing an AJAX request with the queryURL
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
-
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
-
-            // Creating and storing a div tag
-            var bandsDiv = $("<div>");
-
-            // Creating a paragraph tag with the result item's rating
-            var para = $("<p>").text("Rating: " + results[i].rating);
-
-            // Creating and storing an image tag
-            var bandImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            bandImage.attr("src", results[i].images.fixed_height.url);
-
-            // Appending the paragraph and image tag to the bandsDiv
-            bandsDiv.append(para);
-            bandsDiv.append(bandImage);
-
-            // Prependng the bandsDiv to the HTML page in the "#gifs-go-here" div
-            $("#gifs-go-here").prepend(bandsDiv);
-        }
-        });
-      });
     });
